80db7b5090538be026425060d09081308c1b2157
Stavros Champilomatis <stavros.champilomatis@booking.com>
2025-05-22 16:54:34 +0200

    fix local dev frontend, add missing libraries in backend

---
+++ frontend/server.js
@@ -0,0 +1,38 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import express from 'express'
+import path from 'path'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import { createServer as createViteServer } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+async function startServer() {
+  const app = express()
+
+  const vite = await createViteServer({
+    root: __dirname, // Ensure correct root
+    server: { middlewareMode: 'html' },
+    base: '/ragpile/',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  })
+
+  app.use('/ragpile/', (req, res, next) => {
+    req.url = req.url.replace(/^\/path/, '') || '/'
+    vite.middlewares.handle(req, res, next)
+  })
+  const port = 5173
+  app.listen(port, () => {
+    console.log(`Dev server running at http://0.0.0.0:${port}/ragpile/`)
+  })
+}
+
+startServer()
