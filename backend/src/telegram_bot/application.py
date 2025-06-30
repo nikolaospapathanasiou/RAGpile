4ba91d1a9f400792bf23fea68fb25d04276e983f
Stavros <schampilomatis@gmail.com>
2025-06-30 22:22:03 +0200

    add echo handler

--- backend/src/telegram_bot/application.py
+++ backend/src/telegram_bot/application.py
@@ -3,7 +3,7 @@ from typing import AsyncContextManager, Callable, Coroutine, cast
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import select
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler
 
 from models import User
 
@@ -13,6 +13,7 @@ def new_telegram_application(
 ) -> Application:
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler("start", start_factory(session_factory)))
+    application.add_handler(MessageHandler(None, echo))
 
     return application
 
@@ -38,3 +39,13 @@ def start_factory(
             await session.commit()
 
     return start
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not update.message:
+        return
+    if not update.effective_chat:
+        return
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text or ""
+    )
